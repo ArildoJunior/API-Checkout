6844bf0efc1731661542d5605223663ec5aad5d8
ArildoJunior <arildoajunior@gmail.com>
2021-05-10 16:00:30 -0300

    Inicio

---
+++ src/test/java/com/arildo/ecommerce/Checkout/CheckoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arildo.ecommerce.Checkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
